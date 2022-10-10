36a19e3520b39f0c7c430aa5976cd5ab995438a3
Viktor-Panasiuk <vo151184pvv@gmail.com>
2022-10-10 14:26:41 +0300

    Do task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listRef = ingredients.map(ingredient => {
+  const liRef = document.createElement('li');
+  liRef.textContent = ingredient;
+  liRef.classList.add('item');
+  return liRef;
+}
+);
+
+const ulRef = document.querySelector('#ingredients');
+ulRef.append(...listRef);
