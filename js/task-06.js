6c62fb02f1a3e154a2466e20369d066a1bb89bd2
Viktor-Panasiuk <vo151184pvv@gmail.com>
2022-10-10 23:24:12 +0300

    Do task-07

---
+++ js/task-06.js
@@ -0,0 +1,11 @@
+
+const validInputText = ({ currentTarget }) => { 
+    const dataLenght = Number(currentTarget.dataset.length);
+    
+    currentTarget.classList.remove('valid');
+    currentTarget.classList.remove('invalid');
+    currentTarget.classList.add((currentTarget.value.length === dataLenght) ? 'valid' : 'invalid');
+};
+
+const inputRef = document.querySelector('#validation-input');
+inputRef.addEventListener('blur', validInputText);
