ec53869130664e9ec921cca4d32017ff558d5a20
Viktor-Panasiuk <vo151184pvv@gmail.com>
2022-10-11 12:05:03 +0300

    Do task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonRef = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+const bodyRef = document.querySelector('body');
+
+buttonRef.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+  const color = getRandomHexColor();
+  spanRef.textContent = color.toString();
+  bodyRef.style.backgroundColor = color;
+}
